2718f5427d91e0effac1da9cc171d5b6f824b39f
abetomo <abe@enzou.tokyo>
2021-08-03 19:54:26 +0900

    fix: fix setup script

--- Rust_pulldown_cmark_wasm/wasm-build.sh
+++ Rust_pulldown_cmark_wasm/wasm-build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+rustup target add wasm32-unknown-unknown
+cargo install wasm-bindgen-cli
+
 cd ./wasm-example
-cargo install
 cargo build --target wasm32-unknown-unknown --release
 wasm-bindgen ./target/wasm32-unknown-unknown/release/wasm_example.wasm --out-dir ./dist --nodejs
 cd ..
